7a81cfbe2892d0063e0a363b75305bd7f4ef792c
Guilherme Sobral <SEUEMAILDOGITHUB>
2026-02-23 15:50:56 -0300

    Migrando clientes para Firestore

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBH1MWR7uSgcOF4WsrQnnkgPNpzdBkonxA",
@@ -7,9 +8,13 @@ const firebaseConfig = {
   projectId: "grsolution-8e6cb",
   storageBucket: "grsolution-8e6cb.firebasestorage.app",
   messagingSenderId: "65708479471",
-  appId: "1:65708479471:web:b6196202e1bf1c269b9c1a"
+  appId: "1:65708479471:web:f9eff0ed0f59bd579b9c1a",
+  measurementId: "G-Y7G3ZG6204"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
+// Inicializa os serviços e exporta
 export const db = getFirestore(app);
+export const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
